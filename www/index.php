7ee7970e37760b30fdef425b93963269e71026be
vlad trygub <vladtrygub83@gmail.com>
2016-05-18 11:45:41 +0300

    updated main index.php required Router.php

--- www/index.php
+++ www/index.php
@@ -1,7 +1,12 @@
 <?php
 
 include("log4php/Logger.php");
-
 Logger::configure('configs/config.xml');
+//$logger = Logger::getLogger('myLogger');
 
-$log = Logger::getLogger('myLogger');
+define('ROOT', dirname(__FILE__));
+
+require_once(ROOT . '/components/Router.php');
+
+$router = new Router();
+$router->run();
