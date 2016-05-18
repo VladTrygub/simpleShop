5596af5949e681b162414f71a59d6c842999e409
vlad trygub <vladtrygub83@gmail.com>
2016-05-18 11:43:49 +0300

    added routes.php

---
+++ www/configs/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+  'products/([a-z]+)' => 'product/category/$1',
+  'products' => 'product/list',
+  'about' => 'about/index',
+  'contact' => 'contact/index',
+  'home' => 'home/index'
+);
