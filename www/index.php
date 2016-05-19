a16ef3b05570fbb1ea64ad6b9509d1b7977c0284
vlad trygub <vladtrygub83@gmail.com>
2016-05-19 21:20:33 +0300

    added requere Db.php

--- www/index.php
+++ www/index.php
@@ -2,11 +2,14 @@
 
 include("log4php/Logger.php");
 Logger::configure('configs/config.xml');
-//$logger = Logger::getLogger('myLogger');
+$logger = Logger::getLogger('myLogger');
 
 define('ROOT', dirname(__FILE__));
 
 require_once(ROOT . '/components/Router.php');
+require_once(ROOT . '/components/Db.php');
 
 $router = new Router();
 $router->run();
+//$logger->info(md5('user_pass2'));
+$logger->info("\n\n===========================================================================================================\n");
