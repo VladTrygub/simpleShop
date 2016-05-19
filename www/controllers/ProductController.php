5521ffaed42880da69caa9ba6edd2c248d6c4f1e
vlad trygub <vladtrygub83@gmail.com>
2016-05-19 21:25:13 +0300

    have done two methods

--- www/controllers/ProductController.php
+++ www/controllers/ProductController.php
@@ -1,13 +1,17 @@
 <?php
 
+include_once ROOT . '/models/Products.php';
+
 class ProductController {
 
   public function actionList() {
+    $productsList = Products::getProductsList();
     require_once(ROOT . '/views/product/index.php');
     return true;
   }
 
-  public function actionCategory($category) {
+  public function actionCategory($categoryName) {
+    $productsList = Products::getProductsListByCategory($categoryName);
     require_once(ROOT . "/views/product/byCategory.php");
     return true;
   }
